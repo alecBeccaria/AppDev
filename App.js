f634210fc87602921f92ebf28f2d215359d9d38a
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-04-15 11:05:49 -0600

    Merge branch 'main' of https://github.com/alecBeccaria/AppDev

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import { StyleSheet, Text, View, Image, Button  } from 'react-native';
 
 function GetThemGifs({search}){
@@ -9,16 +9,16 @@ function GetThemGifs({search}){
       async function getGiphys(search){
         const API_KEY = 'rWOK4M7qED2lP87FJ3tXWfXjrYPKML31';
         let url = "https://api.giphy.com/v1/gifs/search"+
-        "?api_key=rWOK4M7qED2lP87FJ3tXWfXjrYPKML31&q="+ search +"&limit=10&offset=0&rating=R&lang=en";
+        "?api_key=rWOK4M7qED2lP87FJ3tXWfXjrYPKML31&q="+ search +"&limit=10&offset=0&rating=G&lang=en";
         const data = await fetch(url, {method: 'GET'})
         const json = await data.json();
         console.log(json);
-        setGifs(json.data[0].images.downsized_medium.url)
+        setGifs(json.data[rand].images.downsized_medium.url)
       }
       getGiphys(search);
 
       
-  }, [rand]);
+  }, []);
 
   return (
     <View>
@@ -28,17 +28,20 @@ function GetThemGifs({search}){
 }
 
 
-export default function App() {
-    const [refresh, setRefresh] = useState(false);
 
+export default function App() {
+    const [refreshing, setRefreshing] = useState(false);
+    const onRefresh = useCallback(() => {
+      setRefreshing(true);
+      wait(2000).then(() => setRefreshing(false));
+    })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js yeso start working on your app!</Text>
-      <GetThemGifs search="sad"/>
-      <GetThemGifs search="cat"/>
-      <GetThemGifs search="sdog"/>
-      <Button
-      onPress={() => setRefresh(!refresh)} title ="Refresh"></Button>
+      <GetThemGifs search="coding"/>
+      <GetThemGifs search="fun"/>
+      <GetThemGifs search="puppy"/>
+      <Text>Powered by Giphy</Text>
+      <Button onPress={() => {history.go(0)}} title ="Refresh"/>
     </View>
   );
 }
